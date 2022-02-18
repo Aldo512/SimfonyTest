ccf98ea4b000f61cd400c6a29b7e4e07a87d1732
aldo512 <C.g242462536>
2022-02-18 13:23:36 +0100

    Creating entities

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+new \Symfony\WebpackEncoreBundle\WebpackEncoreBundle();
 
 class Kernel extends BaseKernel
 {
